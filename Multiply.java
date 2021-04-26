fac51f37967fe0fdb72ab845ff25601cf9cc0a4d
aaaezx <71643133+aaaezx@users.noreply.github.com>
2021-04-26 12:38:24 +0900

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class Multiply {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		while(true) {
+			System.out.print(">> ");
+			try {
+				int n = s.nextInt();
+			    int m = s.nextInt();
+			    System.out.println(n + "x" + m + "=" + n*m);
+			    break;
+			}
+		    catch(InputMismatchException e){
+			    System.out.println("error");
+			    s.nextLine();
+		    }
+		}
+	}
+}
